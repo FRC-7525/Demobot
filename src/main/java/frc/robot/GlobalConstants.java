8413b7ff052592b235ca4b8f88f4990b9cc3d82b
Aitan Martinez <prisimrobotics@gmail.com>
2026-01-23 16:14:11 -0600

    Inital Commit + Project Setup

---
+++ src/main/java/frc/robot/GlobalConstants.java
@@ -0,0 +1,5 @@
+package frc.robot;
+
+public class GlobalConstants {
+    // Global constants can be defined here
+}
---
+++ src/main/java/frc/robot/Subsystems/Passthrough/Passthrough.java
@@ -0,0 +1,5 @@
+package frc.robot.Subsystems.Passthrough;
+
+public class Passthrough {
+    
+}
