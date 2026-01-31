5d90037b15a2f2419ffcf8fda2fcf2efc0249f88
BSTHOSHAN <bsthoshan@gmail.com>
2026-01-31 16:16:03 -0600

    Reference commit

--- src/main/java/frc/robot/Subsystems/Drive/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive/Drive.java
@@ -1,5 +1,44 @@
 package frc.robot.Subsystems.Drive;
 
+import java.io.File;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
+import swervelib.SwerveDrive;
+import swervelib.SwerveInputStream;
+import swervelib.parser.SwerveParser;
+import static frc.robot.Subsystems.Drive.DriveConstants.*;
+
 public class Drive {
-    
+    private static Drive instance;
+    private SwerveInputStream swerveInputs;
+	private SwerveDrive swerveDrive;
+	private boolean fieldRelative;
+    public final XboxController DRIVER_CONTROLLER;
+
+    public static Drive getInstance() {
+		if (instance == null) {
+			instance = new Drive();
+		}
+		return instance;
+	}
+
+    private Drive() {
+        DRIVER_CONTROLLER = new XboxController(0);
+
+		try {
+			File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+			swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(
+				MAX_SPEED.magnitude(),
+				new Pose2d(9.9, 4.0, Rotation2d.fromDegrees(0))
+			);
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to create SwerveDrive", e);
+		}
+		swerveDrive.setMotorIdleMode(true);
+
+        swerveInputs = SwerveInputStream.of(swerveDrive, () -> -DRIVER_CONTROLLER.getLeftY(), () -> -DRIVER_CONTROLLER.getLeftX());
+    }
 }
--- src/main/java/frc/robot/Subsystems/Drive/DriveConstants.java
+++ src/main/java/frc/robot/Subsystems/Drive/DriveConstants.java
@@ -1,5 +1,15 @@
 package frc.robot.Subsystems.Drive;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+
 public class DriveConstants {
-    public static double MAX_SPEED = 5.0;
+    public static final LinearVelocity MAX_SPEED = MetersPerSecond.of(4.6);
+	public static final LinearVelocity SLOW_SPEED = MetersPerSecond.of(MAX_SPEED.magnitude() * 0.2);
+	public static final AngularVelocity MAX_ANGULAR_VELOCITY = RotationsPerSecond.of(3);
+	public static final AngularVelocity SLOW_ANGULAR_VELOCITY = RotationsPerSecond.of(MAX_ANGULAR_VELOCITY.magnitude() * 0.2);
+
 }
