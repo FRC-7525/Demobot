50f982977188b0854dceea434e03df50a430f00d
Yuriana <yurianaelenahernandez@gmail.com>
2026-03-07 15:57:04 -0600

    cancoder ID changes

--- src/main/java/frc/robot/Subsystems/Drive/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive/Drive.java
@@ -57,7 +57,7 @@ public class Drive {
 			swerveDrive.drive(swerveInputs.get());
 		}
 
-		robot.setRobotPose(swerveDrive.getSimulationDriveTrainPose().get());
+		
 		SmartDashboard.putData(robot);
 	}
 }
