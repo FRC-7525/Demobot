c67481fc4aed2e8f371d36d193e3620778bf8bf5
Aitan Martinez <prisimrobotics@gmail.com>
2026-03-19 09:38:33 -0400

    Locked in logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -38,6 +39,9 @@ public class Robot extends TimedRobot {
 	 */
 
 	public Robot() {
+		DataLogManager.start();
+		DataLogManager.logNetworkTables(true);
+		DataLogManager.logConsoleOutput(true);
 		AutoBuilderStuff.setConfig();
 		DriverStation.silenceJoystickConnectionWarning(true);
 		CommandScheduler.getInstance().unregisterAllSubsystems();
