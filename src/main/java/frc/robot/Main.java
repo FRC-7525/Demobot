ae7f88e619a01ad2ee65df848984f3c7064a4ff1
Aitan Martinez <prisimrobotics@gmail.com>
2026-09-22 13:31:34 -0500

    Remove crash check

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-import frc.robot.CI.CrashCheck;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
@@ -13,15 +12,14 @@ import frc.robot.CI.CrashCheck;
  * call.
  */
 public final class Main {
+  private Main() {}
 
-	private Main() {}
-
-	/**
-	 * Main initialization function. Do not perform any initialization here.
-	 *
-	 * <p>If you change your main robot class, change the parameter type.
-	 */
-	public static void main(String... args) {
-		RobotBase.startRobot("Crash".equals(System.getenv("CI_NAME")) ? () -> new CrashCheck(new Robot()) : Robot::new);
-	}
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>If you change your main robot class, change the parameter type.
+   */
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
 }
